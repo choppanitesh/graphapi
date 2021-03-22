c33c3f1c86b6ad5bdd5d84f75c457883e8a4f85b
Nitesh Choppa <nitesh.choppa@hrblock.com>
2021-03-22 08:47:24 -0500

    test project

---
+++ GraphDemo.UnitTest/MutationTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using GraphDemo.Entities;
+using GraphDemo.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GraphDemo.UnitTest
+{
+    public class MutationTest
+    {
+        private readonly Mock<ILogger<Mutation>> mockLogger;
+        private Mutation mutation;
+
+        public MutationTest()
+        {
+            mockLogger = new Mock<ILogger<Mutation>>();
+            mutation = new Mutation(mockLogger.Object);
+        }
+
+        [Fact]
+        public async void AddOrderShouldThrowErrorForInvalidInput()
+        {
+            //Arrange
+            var order = new AddOrderInput();
+
+            //Act
+            var mutationStatus = await  Assert.ThrowsAsync<Exception>(() => mutation.AddOrderAsync(order, null));
+
+            //Assert
+            mutationStatus.Message.Should().Be("Bad request");
+        }
+    }
+}
